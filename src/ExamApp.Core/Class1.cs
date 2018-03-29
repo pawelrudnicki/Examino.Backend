988d9b0ca5c81d58a4e88fb0e5ab7a97207a0848
pawelrudnicki <kontakt.prudnicki@gmail.com>
2018-03-29 12:15:59 +0200

    Added Api, Core and Infrastructure

---
+++ src/ExamApp.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ExamApp.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/ExamApp.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ExamApp.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
